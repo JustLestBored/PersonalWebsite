41eed03bdef55ce67f089a51146509b28be57211
JustLestBored <158271641+JustLestBored@users.noreply.github.com>
2024-03-02 08:31:27 +0800

    New/redo UI For the Portfolio
    -Have a New background
    -Rearrange the structure
    - Almost Done in the UI

--- OnlinePortfolio/base/urls.py
+++ OnlinePortfolio/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+app_name='base'
 
 urlpatterns = [
     path ('', views.home, name='home'),
-    
+    path ('About', views.abouts, name='about'),
+    path ('Resume', views.resume, name='resume'),
+    path ('Contact', views.contact, name='contact'),
+    path ('Gallery', views.gallery, name='gallery'),
 ]
--- OnlinePortfolio/base/views.py
+++ OnlinePortfolio/base/views.py
@@ -4,4 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+    
+def abouts(request):
+    return render(request,'About.html')
+    
+def resume(request):
+    return render(request,'resume.html')
 
+def contact(request):
+    return render(request,'contact.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
